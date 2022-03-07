91121f4383735a6643b5e1b666ca0b7308496fde
Freddy Söderberg <freddy.soderberg@edu.edugrade.se>
2022-03-07 15:43:58 +0100

    added routing to login button

--- client/src/containers/LoginContainer.js
+++ client/src/containers/LoginContainer.js
@@ -2,6 +2,7 @@ import React, { Component, useContext, useEffect, useState} from "react"
 import Select from "react-select"
 import { useFetch } from "../customHooks/UseFetch";
 import { UserContext } from "../customHooks/UserContext";
+import { useNavigate } from "react-router-dom";
 
 export function LoginContainer() {
   const {data, isLoading, error} = useFetch("/get_users");
@@ -10,20 +11,31 @@ export function LoginContainer() {
     console.log(data)
 
     const options = [
-        {value: 1, label: 'Robin', authorized: true},
-        {value: 2, label: 'Freddy', authorized: true}
+        {value: 1, label: 'Robin', authorized: false},
+        {value: 2, label: 'Freddy', authorized: false}
     ]
     
     const handleChange = (person) =>{
         setSelectedUser(person)
     }
 
+    let navigate = useNavigate();
+    function routeChange() {
+        let path = "/";
+        navigate(path);
+    }
 
+    function onClickHandle() {
+        return setValue(selectedUser);
+    };
 
     return (
         <div className="container-fluid">
             <Select options={options} onChange={handleChange} />
-            <button onClick={() => {setValue(selectedUser)}}>login</button>
+            <button onClick={() => 
+                {routeChange(); 
+                onClickHandle();
+                }}>login</button>
             
         </div>
     )
