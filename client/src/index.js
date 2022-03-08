0b3f7ff7fc8df86686d2d31d2a600ac76ca4272d
Robin Hörnkvist <robin.hornkvist@edu.edugrade.se>
2022-03-08 08:49:17 +0100

    Made index to a function to setup for a protected router from there

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 
+const Index = () => {
+  return (
+    <App />
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
