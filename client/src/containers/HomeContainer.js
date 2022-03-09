5e3545849cc261c5d7c0e40a21899c673c3827ac
Jonathan Johansson <jonathan.johansson@edu.edugrade.se>
2022-03-09 12:41:01 +0100

    Fixat centreringen av texten på Home och Test sidan

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -4,7 +4,7 @@ import { useAuth } from "../contexts/AuthContext";
 export const HomeContainer = () => {
     const auth = useAuth()
     return (
-        <div className="container-fluid display-1 bg-dark text-white">
+        <div className="container-fluid display-1 bg-dark text-white text-center">
             Välkommen {auth?.user?.label}
             
         </div>
--- client/src/containers/TestContainer.js
+++ client/src/containers/TestContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const TestContainer = () => {
 
     return (
-        <div className="container-fluid bg-dark text-white">
+        <div className="container-fluid bg-dark text-white text-center">
             <h1 className="display-3">Välkommen till testcontainern</h1>
         </div>
     )
