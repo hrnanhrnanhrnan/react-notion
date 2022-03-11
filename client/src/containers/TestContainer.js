b2ce1012034aae0d81b28ed61e8385a1bbc1c3ae
Robin Hörnkvist <robin.hornkvist@edu.edugrade.se>
2022-03-11 15:18:13 +0100

    Added post route on server to create new entry in notion database and added some logic in testcontainer to try the call to the server to ensure that it's working as it should

--- client/src/containers/TestContainer.js
+++ client/src/containers/TestContainer.js
@@ -1,13 +1,52 @@
-import React from "react";
-import {useFetch} from "../customHooks/UseFetch.js"
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext.js";
 
 export const TestContainer = () => {
+    //response is just a state to let show a success or failure message when creating a timereport
+    const [response, setResponse] = useState(null)
+
+    //this gets the user who is logged in 
+    const auth = useAuth()
+
+    //this variabels is used to send to the server endpoint. This variabels is what we then get from the form
+    const date = "2022-03-11"
+    const person = auth.user.value
+    const hours = 3
+    const project = "1a0384cc-414d-4200-8103-97c648f638c9"
+    const note = "coding from lion-org"
     
-    const {data} = useFetch("/get_timereports")
-    console.log(data)
+    //eventhandler to handle the click from the timereport button, this can later be used to connect to the submit button of the form
+    const handleClick = () => {
+        //this is the endpoint of the server, we use the variabels or values 
+        //we get from the form and pass it into the url as parameters 
+        fetch(`/timereports/${date}/${person}/${hours}/${project}/${note}`)
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error("Could not fetch the data...")
+        })
+        .then((jsonResponse) => {
+            //this is just to set the state of the container. this is just testing
+            setResponse(jsonResponse)
+        })
+        .catch((error) => {
+            //this is same as above, only for testing
+            setResponse(error)
+        })
+    }
+    
     return (
         <div className="container-fluid bg-dark text-white text-center">
-            <h1 className="display-3">Välkommen till testcontainern</h1>
+            {!response && <Button onClick={handleClick}>Skapa tidsrapport</Button>}
+            {
+                response?.ok ? (
+                    <h3>{response?.message}</h3>
+                ) : (
+                    <h3>{response?.message}</h3>
+                )
+            }
         </div>
        
     )
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require("dotenv").config()
 const express = require("express")
 const {Client} = require("@notionhq/client")
+const { parse } = require("dotenv")
 const app = express()
 const port = process.env.PORT || 5000
+
+app.use(express.json())
 
 const notion = new Client({
     auth: process.env.NOTION_TOKEN
@@ -53,9 +56,24 @@ app.get("/get_timereports", async (req, res) => {
 })
 
 //POST routes
-
-app.post("/timereports", async (req, res) => {
-  const query = await notion.env.NO
+//the endpoint which is called from the frontend and 
+//everything that follow "/:" is parameters that we populate from the frontend 
+//with variables from the form that is being submitted
+//the values from the frontend can then be accessed through req.params and passed into the updateTimeReport method
+//the endpoint then returns success or failure message back to the frontend
+app.get("/timereports/:date/:personId/:hours/:projectId/:note", async (req, res) => {
+    let message = ""
+    let ok = false
+    try {
+        await updateTimereports(req.params.date, req.params.personId, req.params.hours, req.params.projectId, req.params.note)
+        return message = "Successfully mined some bitcoins!", ok = true
+    }
+    catch(error){
+        return message = error.message
+    }
+    finally {
+        res.send({ok, message})
+    }
 })
 
 //PUT routes
@@ -63,3 +81,46 @@ app.post("/timereports", async (req, res) => {
 
 
 //DELETE routes
+
+
+//Method to update the timereport database through notion API call
+const updateTimereports = async (date, personId, hours, projectId, note) => {
+
+    //sends the parameters from the method call to the api to create new database entry 
+    await notion.pages.create({
+        parent: {database_id: process.env.NOTION_TIMEREPORT_DATABASE_ID},
+        properties: {
+          Date: {
+              date: {
+                  start: date
+              }
+          },
+          Person: {
+              relation: [
+                  {
+                      id: personId
+                  }
+              ]
+          },
+          Hours: {
+              number: parseInt(hours)
+          },
+          Project: {
+              relation: [
+                  {
+                      id: projectId
+                  }
+              ]
+          },
+          Note: {
+              title: [
+                  {
+                      text: {
+                          content: note
+                      }
+                  }
+              ]
+          }
+      }
+      })
+  }
