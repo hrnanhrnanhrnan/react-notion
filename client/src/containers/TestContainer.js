348f5505e6113c5976dd03cb4034fd3014f23d4d
Jonathan Johansson <jonathan.johansson@edu.edugrade.se>
2022-03-15 09:19:05 +0100

    Timerapport added to testcontainer.

--- client/src/containers/TestContainer.js
+++ client/src/containers/TestContainer.js
@@ -3,20 +3,26 @@ import { Button } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext.js";
 
 export const TestContainer = () => {
-    //response is just a state to let show a success or failure message when creating a timereport
+    // //response is just a state to let show a success or failure message when creating a timereport
     const [response, setResponse] = useState(null)
-
-    //this gets the user who is logged in 
+    const [inputs, setInputs] = useState({});
+    // //this gets the user who is logged in 
     const auth = useAuth()
 
-    //this variabels is used to send to the server endpoint. This variabels is what we then get from the form
-    const date = "2022-03-11"
+    // //this variabels is used to send to the server endpoint. This variabels is what we then get from the form
+    // const date = inputs.date
+    // const person = auth.user.value
+    // const hours = inputs.hours
+    // const project = inputs.project
+    // const note = inputs.note
+
+    const date = inputs.date
     const person = auth.user.value
-    const hours = 3
-    const project = "1a0384cc-414d-4200-8103-97c648f638c9"
-    const note = "coding from lion-org"
+    const hours = inputs.hours
+    const project = "a15930f95dad4ebd9dd9879de038191f"
+    const note = inputs.note
     
-    //eventhandler to handle the click from the timereport button, this can later be used to connect to the submit button of the form
+    // eventhandler to handle the click from the timereport button, this can later be used to connect to the submit button of the form
     const handleClick = () => {
         //this is the endpoint of the server, we use the variabels or values 
         //we get from the form and pass it into the url as parameters 
@@ -36,21 +42,104 @@ export const TestContainer = () => {
             setResponse(error)
         })
     }
+
     
+    // // const [values, setValues] = useState({
+    // //     date: "", hours: 0, project: "", note: ""
+    // // });
+    
+    // // const set = (name) => {
+    // //     return ({ target: { value } }) => {
+    // //     setValues((oldValues) => ({ ...oldValues, [name]: value }));
+    // //     };
+    // // };
+    
+    // return (
+    // <form onSubmit={handleClick} className="text-white"> 
+    // <h2>Create Timereport</h2>
+
+    // <label>Date:</label>
+    // <input 
+    //     id="dateLabel"
+    //     type="text" required
+    //     value={values.date} onChange={set("date")}
+    // />
+
+    // <label>Hours:</label>
+    // <input 
+    //     type="number" required
+    //     value={values.hours} onChange={set("hours")}
+    // />
+
+    // <label>Project:</label>
+    // <input
+    //     type="text" required
+    //     value={values.project} onChange={set("project")} 
+    // />
+
+    // <label>Note:</label>
+    // <textarea value={values.note} onChange={set("note")} />
+
+    // {!response && <Button onClick={handleClick}>Skapa tidsrapport</Button>}
+    //             {
+    //                 response?.ok ? (
+    //                     <h3>{response?.message}</h3>
+    //                 ) : (
+    //                     <h3>{response?.message}</h3>
+    //                 )
+    //             }
+    // </form>
+    // );
+
+    const handleChange = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs(values => ({...values, [name]: value}))
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(inputs, person);
+      handleClick();
+    }
+  
     return (
-        <div className="container-fluid bg-dark text-white text-center">
-            {!response && <Button onClick={handleClick}>Skapa tidsrapport</Button>}
-            {
-                response?.ok ? (
-                    <h3>{response?.message}</h3>
-                ) : (
-                    <h3>{response?.message}</h3>
-                )
-            }
-        </div>
-       
+        <div>
+      <form onSubmit={handleSubmit} className="text-white">
+        <label>Date:
+            <input 
+            type="text" 
+            name="date" 
+            value={inputs.date || ""} 
+            onChange={handleChange}
+            />
+        </label>
+        <label>Hours:
+          <input 
+            type="number" 
+            name="hours" 
+            value={inputs.hours || ""} 
+            onChange={handleChange}
+            />
+        </label>
+        <label>Project:
+            <select onChange={handleChange}>
+                <option value="a15930f95dad4ebd9dd9879de038191f">TEST</option>
+                <option value="1a0384cc414d4200810397c648f638c9">My First Project</option>
+                <option value="a501137114ea4dec8238a460cb9c0aa5">Another Project</option>
+                <option value="c7f2f48431324c00a1a8bfe27a2fb1cf">The Best Project</option>
+            </select> 
+        </label>
+        <label>Note:
+            <textarea 
+            name="note" 
+            value={inputs.note || ""} 
+            onChange={handleChange}
+            />
+        </label>
+          <input type="submit" />
+      </form>
+      </div>
     )
-   
-    
 }
 
