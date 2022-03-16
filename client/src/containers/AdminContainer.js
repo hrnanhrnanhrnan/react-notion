4280ddc99e90fe76c3ad75607e347317b908aa1f
Robin Hörnkvist <robin.hornkvist@edu.edugrade.se>
2022-03-16 11:16:45 +0100

    added comments in the timereport container and put the postrequest method directly inside the handlesubmit eventhandler and deleted the handleclick method

--- client/src/containers/AdminContainer.js
+++ client/src/containers/AdminContainer.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { AdminComponent } from "../components/AdminComponent"
 import { useAuth } from "../contexts/AuthContext";
+import { useFetch } from "../customHooks/UseFetch";
 
 export const AdminContainer = () => {
+    const {data: users, isLoading: isLoadingUsers} = useFetch("/get_people")
+    const {data: projects, isLoading: isLoadingProjects} = useFetch("/get_database")
     const auth = useAuth()
+
+    if(!isLoadingUsers && !isLoadingProjects){
+        console.log(users)
+        console.log(projects)
+    }
     return (
         <AdminComponent>
         </AdminComponent>
--- client/src/containers/TimeReportContainer.js
+++ client/src/containers/TimeReportContainer.js
@@ -12,30 +12,33 @@ export const TimeReportContainer = () => {
     // //this gets the user who is logged in 
     const auth = useAuth()
     
-
+    // populates the projects dropdown select in the component when the data has been loaded
     !isLoading && (() => {
         data.results.map((project) => options.push({value: project.id, label: project.properties.Projectname.title[0].plain_text} )) 
     })()
 
-    
-    // eventhandler to handle the click from the timereport button, this can later be used to connect to the submit button of the form
-    const handleClick = () => {
-        postRequest(`/timereports/${inputs.date}/${auth.user.value}/${inputs.hours}/${inputs.project}/${inputs.note}`, {method: "POST"})
-    }
-
+    // eventhandler to handle when value changes in the form controls in the timereportcomponent
+    // the eventhandler updates the input state of the container and uses the spread operator and "values" to populate the state with the previous state, 
+    // and then sets the state with event target name as a key, and the event target value as the value in the key-value pair 
     const handleChange = (event) => {
       setInputs(values => ({...values, [event.target.name]: event.target.value}))
     }
   
+    // eventhandler to handle when the value in the dropdown changes
+    // the eventhandler updates the input state of the container and uses the spread operator and "values" to populate the state with the previous state, 
+    // and then sets the state with event target name as a key, and the event target value as the value in the key-value pair 
     const handleDropmenu = (event) => {
         setInputs(values => ({...values, project: event.value}))
     }
 
+    // eventhandler to handle when the submit button is pressed in the form in the component
+    // the eventhandler calls the postrequest method and passed the values from the input state as parameters to the url to the server
     const handleSubmit = (event) => {
       event.preventDefault();
-      handleClick();
+      postRequest(`/timereports/${inputs.date}/${auth.user.value}/${inputs.hours}/${inputs.project}/${inputs.note}`)
     }
     
+    // Mounts the timereport component and sends the data from container to the component
     return (
         <TimeReportComponent options={options} inputs={inputs} handleChange={handleChange} handleDropmenu={handleDropmenu} handleSubmit={handleSubmit}/>
     )
