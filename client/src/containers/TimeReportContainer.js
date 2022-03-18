62b141ff9d83160bae4920f4151423d4b860fb5d
Robin Hörnkvist <robin.hornkvist@edu.edugrade.se>
2022-03-18 08:53:32 +0100

    added loading spinner when submitting the timereport

--- client/src/containers/TimeReportContainer.js
+++ client/src/containers/TimeReportContainer.js
@@ -7,13 +7,14 @@ import { TimeReportComponent } from "../components/TimeReportComponent.js";
 export const TimeReportContainer = () => {
     // //response is just a state to let show a success or failure message when creating a timereport
     const [inputs, setInputs] = useState({});
-    const {data, isLoading} = useFetch("/get_database");
+    const {data, isLoading: isLoadingData} = useFetch("/get_database");
+    const [loaded, setLoaded] = useState(true)
     const options = []
     // //this gets the user who is logged in 
     const auth = useAuth()
     
     // populates the projects dropdown select in the component when the data has been loaded
-    !isLoading && (() => {
+    !isLoadingData && (() => {
         data.results.map((project) => options.push({value: project.id, label: project.properties.Projectname.title[0].plain_text} )) 
     })()
 
@@ -33,14 +34,18 @@ export const TimeReportContainer = () => {
 
     // eventhandler to handle when the submit button is pressed in the form in the component
     // the eventhandler calls the postrequest method and passed the values from the input state as parameters to the url to the server
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
       event.preventDefault();
-      postRequest(`/timereports/${inputs.date}/${auth.user.value}/${inputs.hours}/${inputs.project}/${inputs.note}`)
-      setInputs({}) 
+      setLoaded(false)
+      const res = await postRequest(`/timereports/${inputs.date}/${auth.user.value}/${inputs.hours}/${inputs.project}/${inputs.note}`)
+      setLoaded(res.ok)
+      if(loaded) {
+        setInputs({})
+      }
     }
     
     // Mounts the timereport component and sends the data and methods from container to the component
     return (
-        <TimeReportComponent options={options} inputs={inputs} handleChange={handleChange} handleDropmenu={handleDropmenu} handleSubmit={handleSubmit}/>
+        <TimeReportComponent loaded={loaded} options={options} inputs={inputs} handleChange={handleChange} handleDropmenu={handleDropmenu} handleSubmit={handleSubmit}/>
     )
 }
--- client/src/utilities/postRequest.js
+++ client/src/utilities/postRequest.js
@@ -1,17 +1,13 @@
 export const postRequest = async (url) => {
-    let response = {}
-    await fetch(url, {method: "POST"})
+    const res = await fetch(url, {method: "POST"})
     .then((response) => {
         if (response.ok) {
             return response.json()
         }
         throw new Error("Could not fetch the data...")
     })
-    .then((jsonResponse) => {
-        response = jsonResponse
-    })
     .catch((error) => {
-        response = error
+        return error.message
     })
-    return response
+    return res
 }
